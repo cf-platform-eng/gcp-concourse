9655a6ae7bdfc1b7d99aa6c90b5c809076dbc03c
guidowb <guidowb@gmail.com>
2016-12-01 14:40:10 -0600

    Use resource for pre-existing managed zone

--- terraform/c0-gcp-pie-small/1_dns.tf
+++ terraform/c0-gcp-pie-small/1_dns.tf
@@ -1,73 +1,83 @@
 ///////////////////////////////////////////////
+//////// Create DNS Zone //////////////////////
+///////////////////////////////////////////////
+
+resource "google_dns_managed_zone" "env_dns_zone" {
+  name        = "cfplatformeng"
+  dns_name    = "${var.pcf_ert_domain}."
+  description = "DNS zone (var.pcf_ert_domain)for the var.gcp_terraform_prefix deployment"
+}
+
+///////////////////////////////////////////////
 //////// Add DNS Zone Records /////////////////
 ///////////////////////////////////////////////
 
 resource "google_dns_record_set" "ops-manager-dns" {
-  name       = "opsman.${var.pcf_ert_domain}."
+  name       = "opsman.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type       = "A"
   ttl        = 300
 
-  managed_zone = "${var.gcp_managed_zone}"
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
 
   rrdatas = ["${var.pub_ip_opsman}"]
 }
 
 resource "google_dns_record_set" "wildcard-sys-dns" {
-  name       = "*.sys.${var.pcf_ert_domain}."
+  name       = "*.sys.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type       = "A"
   ttl        = 300
 
-  managed_zone = "${var.gcp_managed_zone}"
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
 
   rrdatas = ["${var.pub_ip_global_pcf}"]
 }
 
 resource "google_dns_record_set" "wildcard-apps-dns" {
-  name       = "*.cfapps.${var.pcf_ert_domain}."
+  name       = "*.cfapps.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type       = "A"
   ttl        = 300
 
-  managed_zone = "${var.gcp_managed_zone}"
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
 
   rrdatas = ["${var.pub_ip_global_pcf}"]
 }
 
 resource "google_dns_record_set" "app-ssh-dns" {
-  name       = "ssh.sys.${var.pcf_ert_domain}."
+  name       = "ssh.sys.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type       = "A"
   ttl        = 300
 
-  managed_zone = "${var.gcp_managed_zone}"
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
 
   rrdatas = ["${var.pub_ip_ssh_and_doppler}"]
 }
 
 resource "google_dns_record_set" "doppler-dns" {
-  name       = "doppler.sys.${var.pcf_ert_domain}."
+  name       = "doppler.sys.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type       = "A"
   ttl        = 300
 
-  managed_zone = "${var.gcp_managed_zone}"
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
 
   rrdatas = ["${var.pub_ip_ssh_and_doppler}"]
 }
 
 resource "google_dns_record_set" "loggregator-dns" {
-  name       = "loggregator.sys.${var.pcf_ert_domain}."
+  name       = "loggregator.sys.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type       = "A"
   ttl        = 300
 
-  managed_zone = "${var.gcp_managed_zone}"
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
 
     rrdatas = ["${var.pub_ip_ssh_and_doppler}"]
 }
 
 resource "google_dns_record_set" "tcp-dns" {
-  name       = "tcp.${var.pcf_ert_domain}."
+  name       = "tcp.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type       = "A"
   ttl        = 300
 
-  managed_zone = "${var.gcp_managed_zone}"
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
 
   rrdatas = ["${var.pub_ip_ssh_tcp_lb}"]
 }

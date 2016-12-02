4bd26b1e1b67a58e619bfb9d30f9b787238a6b36
guidowb <guidowb@gmail.com>
2016-12-02 16:07:29 -0600

    Print available products in case we don't find what we expect

--- ci/tasks/tiles/upload-tile.sh
+++ ci/tasks/tiles/upload-tile.sh
@@ -11,7 +11,7 @@ sudo cp tool-om/om-linux /usr/local/bin
 sudo chmod 755 /usr/local/bin/om-linux
 
 echo "=============================================================================================="
-echo " Uploading MySQL tile to @ https://opsman.$pcf_ert_domain ..."
+echo " Uploading ${product} tile to @ https://opsman.$pcf_ert_domain ..."
 echo "=============================================================================================="
 
 ##Upload p-mysql Tile
@@ -29,7 +29,13 @@ uaac target https://${opsman_host}/uaa --skip-ssl-validation > /dev/null 2>&1
 uaac token owner get opsman ${pcf_opsman_admin} -s "" -p ${pcf_opsman_admin_passwd} > /dev/null 2>&1
 export opsman_bearer_token=$(uaac context | grep access_token | awk -F ":" '{print$2}' | tr -d ' ')
 
+##Find most recent avaiable product version
 product_version=$(curl -s -k -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${opsman_bearer_token}" "https://${opsman_host}/api/v0/available_products" | jq ' .[] | select ( .name == "${product}") | .product_version ' | tr -d '"')
+if [ -z "${product_version}" ]; then
+	echo "Error: ${product} not found"
+	echo "Available products are:"
+	curl -s -k -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${opsman_bearer_token}" "https://${opsman_host}/api/v0/available_products" | jq ' .[] | .name '
+fi
 
 ##Move 'available product to 'staged'
 

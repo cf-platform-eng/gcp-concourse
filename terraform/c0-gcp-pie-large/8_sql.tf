4b2b7f2d94b3eb7f9728fb96d228beb2335e0338
guidowb <guidowb@gmail.com>
2017-06-06 08:06:57 -0500

    Added new databases in 1.11

--- terraform/c0-gcp-pie-large/8_sql.tf
+++ terraform/c0-gcp-pie-large/8_sql.tf
@@ -133,3 +133,27 @@ resource "google_sql_database" "diego" {
 
   count = "1"
 }
+
+resource "google_sql_database" "locket" {
+  name       = "locket"
+  instance   = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.diego"]
+
+  count = "1"
+}
+
+resource "google_sql_database" "silk" {
+  name       = "silk"
+  instance   = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.locket"]
+
+  count = "1"
+}
+
+resource "google_sql_database" "nfsvolume" {
+  name       = "nfsvolume"
+  instance   = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.silk"]
+
+  count = "1"
+}
